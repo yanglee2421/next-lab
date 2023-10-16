8863aa2b7a79e32f91e6b155fc2b5f97399f32c0
yanglee2421 <yanglee2421@gmail.com>
2023-10-16 22:23:23 +0800

    init

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  eslint: {
+    // ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // ignoreBuildErrors: true,
+  },
+  i18n: {
+    locales: ["en_US", "zh_CN", "zh_TW"],
+    defaultLocale: "en_US",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
