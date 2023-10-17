4324c156d3dd3c19e7fb04fac4dab66439b72fb9
ZhangSheng <yanglee2421@gmail.com>
2023-10-17 11:03:27 +0800

    router loader

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import "@/styles/globals.css";
 
 // NextJs Imports
 import type { AppProps } from "next/app";
+import { Router } from "next/router";
 
 // I18n Imports
 import { appWithTranslation } from "next-i18next";
@@ -15,13 +16,27 @@ import { ThemeProvider } from "@/theme";
 // Toast Imports
 import { Toaster } from "react-hot-toast";
 
+// NProgress Imports
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+
+Router.events.on("routeChangeStart", () => {
+  NProgress.start();
+});
+Router.events.on("routeChangeError", () => {
+  NProgress.done();
+});
+Router.events.on("routeChangeComplete", () => {
+  NProgress.done();
+});
+
 export default appWithTranslation(({ Component, pageProps }: AppProps) => {
   return (
     <ReduxProvider>
       <QueryProvider>
         <ThemeProvider>
           <Toaster />
-          <Component {...pageProps} />;
+          <Component {...pageProps} />
         </ThemeProvider>
       </QueryProvider>
     </ReduxProvider>
