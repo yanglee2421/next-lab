81ee669780e58d20b4918089543a651b0f01ad4d
yanglee2421 <yanglee2421@gmail.com>
2023-10-30 22:45:58 +0800

    tailwindcss config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,31 +1,35 @@
+// Tailwindcss Imports
 import type { Config } from "tailwindcss";
 
-const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+export default defineConfig();
+
+function defineConfig(): Config {
+  return {
+    content: [
+      "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+      "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+      "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+      extend: {
+        backgroundImage: {
+          "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+          "gradient-conic":
+            "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        },
+      },
+      screens: {
+        sm: "600px",
+        md: "900px",
+        lg: "1200px",
+        xl: "1536px",
+        "2xl": "1920px",
       },
     },
-    screens: {
-      sm: "600px",
-      md: "900px",
-      lg: "1200px",
-      xl: "1536px",
-      "2xl": "1920px",
+    plugins: [],
+    important: "#__next",
+    corePlugins: {
+      preflight: true,
     },
-  },
-  plugins: [],
-  important: "#__next",
-  corePlugins: {
-    preflight: true,
-  },
-};
-export default config;
+  };
+}
