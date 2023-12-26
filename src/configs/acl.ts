d35c53657d5dd38ea3d3bb9a521753b1695b817d
ZhangSheng <yanglee2421@gmail.com>
2023-12-26 18:24:57 +0800

    fix: update acl config

--- src/configs/acl.ts
+++ src/configs/acl.ts
@@ -1,48 +1,51 @@
+// ACL Imports
 import {
   AbilityBuilder,
   createMongoAbility,
   MongoAbility,
 } from "@casl/ability";
 
-export type Subjects = string;
-export type Actions = "manage" | "create" | "read" | "update" | "delete";
-export type AppAbility = MongoAbility<[Actions, Subjects]>;
-
-export type ACLObj = {
-  action: Actions;
-  subject: string;
-};
-
 /**
  * Please define your own Ability rules according to your app requirements.
  * We have just shown Admin and Client rules for demo purpose where
  * admin can manage everything and client can just visit ACL page
  */
-const defineRulesFor = (role: string, subject: string) => {
-  const { can, rules } = new AbilityBuilder<AppAbility>(createMongoAbility);
+export function buildAbilityFor(role: number): AppAbility {
+  const acl = new AbilityBuilder<AppAbility>(createMongoAbility);
 
-  if (role === "admin") {
-    can("manage", "all");
-  } else if (role === "client") {
-    can(["read"], "acl-page");
-  } else {
-    can(["read", "create", "update", "delete"], subject);
+  switch (role) {
+    // Admin
+    case 2:
+      acl.can("manage", "all");
+      break;
+
+    // User
+    case 1:
+      acl.can("read", "all");
+      acl.can("read", "acl-page");
+      acl.can("create", "ticket");
+      acl.cannot("read", "page-billing-plan");
+      acl.cannot("update", "subscription");
+      break;
+
+    // Visitor
+    default:
+      acl.can("read", "all");
+      acl.cannot("read", "page-billing-plan");
+      acl.cannot("update", "subscription");
   }
 
-  return rules;
-};
-
-export const buildAbilityFor = (role: string, subject: string): AppAbility => {
-  const acl = new AbilityBuilder<AppAbility>(createMongoAbility).build();
-
-  acl.update(defineRulesFor(role, subject));
-
-  return acl;
-};
+  return acl.build();
+}
 
 export const defaultACLObj: ACLObj = {
-  action: "manage",
-  subject: "all",
+  action: "read",
+  subject: "fallback",
 };
 
-export default defineRulesFor;
+export type AppAbility = MongoAbility<[string, string]>;
+
+export type ACLObj = {
+  action: string;
+  subject: string;
+};
