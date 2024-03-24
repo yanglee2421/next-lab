9edff7f389c9ba6c04975985875d84e42efa50e4
yanglee2421 <yanglee2421@gmail.com>
2024-03-24 23:44:14 +0800

    feat: add jwt

--- src/app/api/post/route.ts
+++ src/app/api/post/route.ts
@@ -1,20 +1,26 @@
-import type { NextRequest} from 'next/server';
+import type { NextRequest } from 'next/server'
 import { NextResponse } from 'next/server'
-
+import { verify } from 'jsonwebtoken'
 import { prisma } from '@/data/prisma'
 
 export async function GET(request: NextRequest) {
-  console.log(request)
+  const authorization = request.headers.get('Authorization')
+  const jwt = authorization?.split(' ').at(-1)
+  if (!jwt) {
+    return NextResponse.error()
+  }
+
+  try {
+    const user = verify(jwt, 'sec')
+  } catch (error) {}
 
   const data = await prisma.post.findMany()
 
-  
-return NextResponse.json({ data })
+  return NextResponse.json({ data })
 }
 
 export async function POST(request: NextRequest) {
   const data = await request.json()
 
-  
-return NextResponse.json({ data })
+  return NextResponse.json({ data })
 }
